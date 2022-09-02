0620e51537f8c3c17bccfe3e441fbf0d88a57506
Prashant Dey <prashantsavior@gmail.com>
2022-09-02 14:53:46 +0530

    ADD: websocket express

---
+++ websocket-express/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+const server = require('http').createServer(app)
+
+const io = require('socket.io')(server, {cors: {origin: "*"}});
+
+
+server.listen(4000, () => {
+    console.log('Server Started..');
+});
+
+app.get('/chat', (req,res)=>{
+    res.sendFile(__dirname+'/public/index.html')
+})
+
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('socket connected..', socket.id);
+
+    socket.on('chat', (data) => {
+        console.log(data)
+        // io.sockets.emit('chat', data);
+        socket.broadcast.emit('chat', data)
+    });
+
+});

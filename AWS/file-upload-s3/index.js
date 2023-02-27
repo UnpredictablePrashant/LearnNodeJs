8a016df229b9d2b50cf74e031afa64ae68efad33
Prashant Dey <prashantsavior@gmail.com>
2023-02-27 09:33:54 +0530

    WIP: add the retrieving route for image

--- AWS/file-upload-s3/index.js
+++ AWS/file-upload-s3/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { S3Client } = require('@aws-sdk/client-s3');
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
 
 const dotenv = require('dotenv')
 const path = require('path')
@@ -13,46 +13,73 @@ const app = express()
 
 
 let s3 = new S3Client({
-    region: process.env.AWS_REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    },
-    sslEnabled: false,
-    s3ForcePathStyle: true,
-    signatureVersion: 'v4',
-  });
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+  sslEnabled: false,
+  s3ForcePathStyle: true,
+  signatureVersion: 'v4',
+});
 
 const upload = multer({
-    storage: multerS3({
-      s3: s3,
-      bucket: process.env.AWS_BUCKET_NAME,
-      metadata: function (req, file, cb) {
-        cb(null, {fieldName: file.fieldname});
-      },
-      key: function (req, file, cb) {
-        cb(null, Date.now().toString()+path.parse(file.originalname).name+ path.extname(file.originalname))
-      }
-    })
-  })
-
-
-
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname+'/views/index.html')
-})
-
-app.post('/upload',upload.single("imgFile"),(req,res)=>{
-    try{
-        console.log(req.file)
-        console.log(req.file.location)
-        res.send({'result':'Uploaded'})
-    }catch{
-        res.send('Something went wrong')
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString() + path.parse(file.originalname).name + path.extname(file.originalname))
     }
-    
+  })
 })
 
-app.listen(3000,()=>{
-    console.log('Server started at http://localhost:3000')
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+
+app.get('/api/directimage/:key', async (req, res) => {
+  try {
+    const getObjectParams = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: req.params.key
+    };
+    const getObjectCommand = new GetObjectCommand(getObjectParams);
+    const objectData = await s3.send(getObjectCommand);
+
+    res.set('Content-Type', objectData.ContentType);
+    res.set('Content-Length', objectData.ContentLength);
+    objectData.Body.pipe(res);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'Failed to retrieve file from S3' });
+  }
+});
+
+app.get('/:key', (req, res) => {
+  res.send(`
+        <img src="/api/directimage/${req.params.key}" alt="Image">
+  `);
+});
+
+
+
+app.post('/upload', upload.single("imgFile"), (req, res) => {
+  try {
+    console.log(req.file)
+    console.log(req.file.location)
+    res.send({ 'result': 'Uploaded' })
+  } catch {
+    res.send('Something went wrong')
+  }
+
+})
+
+app.listen(3000, () => {
+  console.log('Server started at http://localhost:3000')
 })
